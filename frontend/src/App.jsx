39a15e370dfac820ec4721a885ba172c0e6a035d
Andy Malak <andymalak1@gmail.com>
2025-12-14 00:56:10 +0000

    Added /518-Capstone to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
           <Routes>
             <Route index element={<Home />} />
             <Route path="/" element={<Home />} />
+            <Route path="/518-Capstone" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
             <Route path="/profile" element={<Profile />} />
